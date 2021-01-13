04effb3198926e70387d55b629263d371097f53d
Kouji Tsushima <77184600+Shirokansen@users.noreply.github.com>
2021-01-13 11:41:06 +0800

    The best messaging app version 1.0

---
+++ app/src/main/java/com/example/tsushima_imapp/Register.java
@@ -0,0 +1,14 @@
+package com.example.tsushima_imapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
